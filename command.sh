7d3bb1c03b348b330e01f437a416ccf336475444
ALI <ali123rahiqi@gmail.com>
2025-04-28 20:30:32 +0100

    done

--- command.sh
+++ command.sh
@@ -8,17 +8,6 @@ docker exec -it --user root spark-master bash
 docker exec -it spark-master spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1 /project/consumer.py
 
 
-
-grafana : http://localhost:3000
-spark : http://localhost:8081
-
-
-
-DESCRIBE KEYSPACE fintech;
-SELECT * FROM fintech.trades LIMIT 10;
-SELECT * FROM fintech.trade_aggregations LIMIT 10;
-
-
 Cassandra: """docker exec -it cassandra cqlsh
             CREATE KEYSPACE IF NOT EXISTS disaster WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
             USE disaster;
@@ -35,91 +24,43 @@ kafka :"""create topic:
         docker exec -it kafka kafka-console-consumer --topic test-topic --bootstrap-server kafka:9092 --from-beginning"""
 
 
+-------------------------------------------------------------------------------------------------------------------------------------------
+Cassandra:
 
-
-------------------------------------------------------------------------------------
-Runs /opt/bitnami/spark/sbin/start-master.sh & to start the master in the background(script starts the Spark master, binding it to port 7077)
-------------------------------------------------------------------------------------
-The global producer statement in producer() declares the producer variable as global, allowing it to be accessed and modified across functions. It’s used to share the Kafka Producer instance
-
-
-
-
-
-
-kubectl delete -f finstream-zookeeper.yaml
-kubectl apply -f finstream-zookeeper.yaml
-kubectl apply -f finstream-cassandra.yaml
-kubectl logs -n finstream cassandra-665b4cb8cf-n8z5v
-kubectl get pods -n finstream
-kubectl get deploy -n finstream
-kubectl get services -n finstream
-
-
-
--Verify Cassandra Connectivity:
- kubectl exec -it cassandra-665b4cb8cf-n8z5v -n finstream -- nodetool status
- kubectl exec -it cassandra-665b4cb8cf-n8z5v -n finstream -- cqlsh 10.244.0.125 9042
  kubectl exec -it cassandra-665b4cb8cf-s7grs -n finstream -- cqlsh cassandra.finstream.svc.cluster.local 9042
+  DESCRIBE KEYSPACE fintech;
+  SELECT * FROM fintech.trades LIMIT 10;
+  SELECT * FROM fintech.trade_aggregations LIMIT 10;
 
 
-
+-------------------------------------------------------------------------------------------------------------------------------------------
 streamlit:
-  kubectl create configmap streamlit-app --from-file=app.py -n finstream
-  kubectl cp app.py finstream/streamlit-59cb9d7897-jsr6b:/app/app.py
+
   kubectl port-forward svc/streamlit 8501:8501 -n finstream
 
+-------------------------------------------------------------------------------------------------------------------------------------------
+Kafka:
 
-🔹 `kubectl create configmap`: you're creating a **ConfigMap** (a way to store non-sensitive files/configs in Kubernetes)  
-🔹 `project-files`: the **name** of the ConfigMap  
-🔹 `--from-file=consumer.py`: you're including the file `consumer.py` into the ConfigMap  
-🔹 `-n finstream`: you're putting the ConfigMap in the **namespace `finstream`**
-
-👉 This lets you **mount `consumer.py` into a Pod** later without baking it into an image.
-
-
-
--Test Kafka:
-  Create a test pod to interact with Kafka:  kubectl run kafka-client --restart=Never --image=bitnami/kafka:3.5.1 --namespace=finstream --command -- sleep infinity
-  Access the pod: kubectl exec -it kafka-client -n finstream -- bash
-  Create a topic:  /opt/bitnami/kafka/bin/kafka-topics.sh --create --bootstrap-server kafka:9092 --replication-factor 1 --partitions 1 --topic test
-  Produce a message:  /opt/bitnami/kafka/bin/kafka-console-producer.sh --bootstrap-server kafka:9092 --topic test
-  Consume the message:  /opt/bitnami/kafka/bin/kafka-console-consumer.sh --bootstrap-server kafka:9092 --topic test --from-beginning
-  Clean up:  kubectl delete pod kafka-client -n finstream
+  Create_a_test_pod_to_interact_with_Kafka:  kubectl run kafka-client --restart=Never --image=bitnami/kafka:3.5.1 --namespace=finstream --command -- sleep infinity
+  Access_the_pod: kubectl exec -it kafka-client -n finstream -- bash
+  Create_a_topic:  /opt/bitnami/kafka/bin/kafka-topics.sh --create --bootstrap-server kafka:9092 --replication-factor 1 --partitions 1 --topic test
+  Produce_a_message:  /opt/bitnami/kafka/bin/kafka-console-producer.sh --bootstrap-server kafka:9092 --topic test
+  Consume_the_message:  /opt/bitnami/kafka/bin/kafka-console-consumer.sh --bootstrap-server kafka:9092 --topic finnhub_trades --from-beginning
+  Clean_up:  kubectl delete pod kafka-client -n finstream
 
 
 
-kubectl create configmap project-files \
-  --from-file=producer.py \
-  --from-file=consumer.py \
-  --from-file=.env \
-  --from-file=config/config.py \
-  --from-file=config/config.yml \
-  -n finstream --dry-run=client -o yaml | kubectl apply -f -
+
+  kubectl exec -it -n finstream kafka-5cf94b6fc6-8dnf2 -- kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic finnhub_trades
+  kubectl exec -it -n finstream kafka-5cf94b6fc6-8dnf2 -- kafka-topics.sh --bootstrap-server kafka:9092 --create --topic finnhub_trades --partitions 1 --replication-factor 1
 
 
+-------------------------------------------------------------------------------------------------------------------------------------------
+spark:
 
-kubectl describe configmap project-files -n finstream
-
-
-ConfigMapis read-only in Kubernetes
-
-
-
-we just need to restart producer to connect to stable kafka 
-
-
-
+kubectl exec -it spark-master-7f5784f74-htp48 -n finstream -- /opt/bitnami/spark/bin/spark-submit     --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,com.datastax.spark:spark-cassandra-connector_2.12:3.5.1,com.github.jnr:jnr-posix:3.1.15     --executor-memory 2g     --executor-cores 4     /project/consumer.py
 
 kubectl exec -it <new-master-pod> -n finstream -- bash
-/opt/bitnami/spark/bin/spark-submit \
-  --master spark://spark-master:7077 \
-  --conf spark.jars.ivy=/tmp/ivy2 \
-  --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.4.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0 \
-  /project/consumer.py
-
-
-
   /opt/bitnami/spark/bin/spark-submit \
   --master spark://spark-master:7077 \
   --conf spark.jars.ivy=/tmp/ivy2 \
@@ -131,56 +72,25 @@ kubectl exec -it <new-master-pod> -n finstream -- bash
   /project/consumer.py
 
 
+  kubectl exec -it spark-master-7f5784f74-htp48 -n finstream -- pkill -f spark-submit
+  kubectl exec -it spark-master-7f5784f74-htp48 -n finstream -- rm -rf /tmp/checkpoint
 
+-------------------------------------------------------------------------------------------------------------------------------------------
+configmap:
 
-
-  kubectl get configmap project-files -n finstream -o yaml > current-configmap.yaml
-
-
-  kubectl create configmap project-files \
+kubectl create configmap project-files \
   --from-file=producer.py=/home/ali/Desktop/FinStream/producer.py \
   --from-file=consumer.py=/home/ali/Desktop/FinStream/consumer.py \
   --from-file=.env=/home/ali/Desktop/FinStream/.env \
   --from-file=__init__.py=/home/ali/Desktop/FinStream/config/__init__.py \
   --from-file=config.py=/home/ali/Desktop/FinStream/config/config.py \
   --from-file=config.yml=/home/ali/Desktop/FinStream/config/config.yml \
-  -n finstream --dry-run=client -o yaml | kubectl apply -f -
+  -n finstream --dry-run=client -o yaml > project-files-configmap.yaml
+
+kubectl create configmap streamlit-app \
+  --from-file=app.py=/home/ali/Desktop/FinStream/app.py \
+  -n finstream --dry-run=client -o yaml > streamlit-app-configmap.yaml
 
 
 
-kubectl exec -it spark-worker-85bcbf7fff-lkfjl  -n finstream -- /opt/bitnami/python/bin/pip3 install cassandra-driver
-
-
-
-
-1- configmap (remember there s  two  )
-2- remember pods have container and also you need to enter the container and run the script for consumer
-
-
-kubectl exec -it -n finstream kafka-5cf94b6fc6-8dnf2 -- kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic finnhub_trades
-kubectl exec -it -n finstream kafka-5cf94b6fc6-8dnf2 -- kafka-topics.sh --bootstrap-server kafka:9092 --create --topic finnhub_trades --partitions 1 --replication-factor 1
-kubectl exec -it -n finstream spark-master-7f5784f74-htp48 -- rm -rf /tmp/checkpoint/agg
-kubectl exec -it -n finstream spark-master-7f5784f74-htp48 -- rm -rf /tmp/checkpoint/raw
-
-
-
-
-
-kubectl exec -it spark-master-7f5784f74-htp48 -n finstream -- /opt/bitnami/spark/bin/spark-submit     --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,com.datastax.spark:spark-cassandra-connector_2.12:3.5.1,com.github.jnr:jnr-posix:3.1.15     --executor-memory 2g     --executor-cores 4     /project/consumer.py
-
-
-kubectl exec -it spark-master-7f5784f74-htp48 -n finstream -- pkill -f spark-submit
-kubectl exec -it spark-master-7f5784f74-htp48 -n finstream -- rm -rf /tmp/checkpoint
-
-
-
-- **Event Time**: The timestamp in the data when the event occurred (e.g., `trade.t`, like `2025-04-27 19:41:09`).
-- **Ingestion Time**: The time Spark processes the data (e.g., `2025-04-27 23:34:54`).
-- **Watermark Difference**: Watermark (e.g., 10 seconds) sets the max gap between event time and the latest event time processed. If event time is too old (e.g., >10 seconds behind), it’s ignored.
-- **Key Issue**: Event time must be close to the latest event time, not ingestion time, to avoid being dropped by the watermark.
-
-
-
-
-
-kubectl apply -f configmaps.yaml -n finstream
+kubectl describe configmap project-files -n finstream
--- consumer.py
+++ consumer.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json, current_timestamp, avg, count, window
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType, LongType , ArrayType
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, LongType, ArrayType
 import logging
 from cassandra.cluster import Cluster, NoHostAvailable
 import uuid
@@ -12,7 +12,7 @@ def store_cassandra_raw(batch_df, batch_id):
     cluster = None
     try:
         logger.info(f'Storing raw batch {batch_id} in Cassandra')
-        cluster = Cluster(['cassandra'], port=9042)
+        cluster = Cluster(['cassandra.finstream.svc.cluster.local'], port=9042)
         session = cluster.connect()
 
         session.execute("CREATE KEYSPACE IF NOT EXISTS fintech WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}")
@@ -24,16 +24,14 @@ def store_cassandra_raw(batch_df, batch_id):
             price double,
             volume double,
             trade_timestamp bigint,
-            ingestion_time timestamp
-        )""")
+            ingestion_time timestamp)""")
 
         df = batch_df.select(
             col("symbol").alias("symbol"),
             col("price").alias("price"),
             col("volume").alias("volume"),
             col("timestamp").alias("trade_timestamp"),
-            current_timestamp().alias("ingestion_time")
-        )
+            current_timestamp().alias("ingestion_time"))
 
         logger.info(f'Starting to insert raw data for batch {batch_id}')
         rows = df.collect()
@@ -41,8 +39,7 @@ def store_cassandra_raw(batch_df, batch_id):
             session.execute(
                 """INSERT INTO fintech.trades (id, symbol, price, volume, trade_timestamp, ingestion_time)
                 VALUES (%s, %s, %s, %s, %s, %s)""",
-                (uuid.uuid4(), row["symbol"], row["price"], row["volume"], row["trade_timestamp"], row["ingestion_time"])
-            )
+                (uuid.uuid4(), row["symbol"], row["price"], row["volume"], row["trade_timestamp"], row["ingestion_time"]))
         logger.info(f'Inserted {len(rows)} raw rows into Cassandra for batch {batch_id}')
 
     except NoHostAvailable as e:
@@ -61,7 +58,7 @@ def store_cassandra_aggregated(batch_df, batch_id):
             logger.info(f'No data in aggregated batch {batch_id}')
             return
 
-        cluster = Cluster(['cassandra'], port=9042)
+        cluster = Cluster(['cassandra.finstream.svc.cluster.local'], port=9042)
         session = cluster.connect()
         session.set_keyspace("fintech")
 
@@ -71,16 +68,14 @@ def store_cassandra_aggregated(batch_df, batch_id):
             symbol text,
             avg_price double,
             trade_count bigint,
-            PRIMARY KEY (window_start, symbol)
-        )""")
+            PRIMARY KEY (window_start, symbol))""")
 
         df = batch_df.select(
             col("window.start").alias("window_start"),
             col("window.end").alias("window_end"),
             col("symbol").alias("symbol"),
             col("avg_price").alias("avg_price"),
-            col("trade_count").alias("trade_count")
-        )
+            col("trade_count").alias("trade_count"))
 
         logger.info(f'Starting to insert aggregated data for batch {batch_id}')
         rows = df.collect()
@@ -88,8 +83,7 @@ def store_cassandra_aggregated(batch_df, batch_id):
             session.execute(
                 """INSERT INTO fintech.trade_aggregations (window_start, window_end, symbol, avg_price, trade_count)
                 VALUES (%s, %s, %s, %s, %s)""",
-                (row["window_start"], row["window_end"], row["symbol"], row["avg_price"], row["trade_count"])
-            )
+                (row["window_start"], row["window_end"], row["symbol"], row["avg_price"], row["trade_count"]))
         logger.info(f'Inserted {len(rows)} aggregated rows into Cassandra for batch {batch_id}')
 
     except NoHostAvailable as e:
@@ -104,7 +98,7 @@ def consumer():
     spark = None
     try:
         logger.info('Starting the consumer')
-        spark = SparkSession.builder.appName("FinStreamProcessing").config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1").getOrCreate()
+        spark = SparkSession.builder.appName("FinStreamProcessing").config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,com.datastax.spark:spark-cassandra-connector_2.12:3.5.1,com.github.jnr:jnr-posix:3.1.15").config("spark.cassandra.connection.host", "cassandra.finstream.svc.cluster.local").config("spark.cassandra.connection.port", "9042").getOrCreate()
         logger.info('Spark session created successfully')
 
         schema = StructType([
@@ -118,7 +112,7 @@ def consumer():
             StructField("type", StringType(), True)
         ])
 
-        df = spark.readStream.format('kafka').option("kafka.bootstrap.servers", config.server).option("subscribe", config.topic_name).option("startingOffsets", "earliest").option("group.id", "finstream-group").load()
+        df = spark.readStream.format('kafka').option("kafka.bootstrap.servers", 'kafka:9092').option("subscribe", 'finnhub_trades').option("startingOffsets", "earliest").option("group.id", "finstream-group").load()
 
         df = df.select(from_json(col("value").cast("string"), schema).alias("data")) \
                .selectExpr("explode(data.data) as trade") \
@@ -129,11 +123,9 @@ def consumer():
                    col("trade.t").alias("timestamp")
                )
 
-        # First Query: Write raw data to Cassandra
         raw_query = df.writeStream.foreachBatch(store_cassandra_raw).outputMode('append').start()
         logger.info('Raw data streaming query started')
 
-        # Second Query: Aggregations every 5 seconds
         aggregated_df = df.groupBy(window((col("timestamp") / 1000).cast("timestamp"), "5 seconds"),col("symbol")).agg(avg("price").alias("avg_price"),count("*").alias("trade_count"))
 
         agg_query = aggregated_df.writeStream.foreachBatch(store_cassandra_aggregated).outputMode('update').start()
@@ -146,6 +138,5 @@ def consumer():
     finally:
         if spark:
             spark.stop()
-
 
 consumer()
